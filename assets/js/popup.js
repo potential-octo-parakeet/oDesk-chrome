4b3c69eaf63a533a57cb8a5eb2af5bcb60be2d45
Mar Cejas <mar@mvcejas.com>
2014-03-15 22:07:01 +0800

    fix syntax error

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -20,23 +20,23 @@ $(function() {
 
     switch (oChrome.getTab()) {
         case 'inbox':
-            $('a[href="#inbox"').parent('li').addClass('active');
+            $('a[href="#inbox"]').parent('li').addClass('active');
             $('#inbox').addClass('active');
             break;
         case 'tickets':
-            $('a[href="#tickets"').parent('li').addClass('active');
+            $('a[href="#tickets"]').parent('li').addClass('active');
             $('#tickets').addClass('active');
             break;
         case 'disputes':
-            $('a[href="#disputes"').parent('li').addClass('active');
+            $('a[href="#disputes"]').parent('li').addClass('active');
             $('#disputes').addClass('active');
             break;
         case 'notices':
-            $('a[href="#notices"').parent('li').addClass('active');
+            $('a[href="#notices"]').parent('li').addClass('active');
             $('#notices').addClass('active');
             break;
         default:
-            $('a[href="#inbox"').parent('li').addClass('active');
+            $('a[href="#inbox"]').parent('li').addClass('active');
             $('#inbox').addClass('active');
     }
 
